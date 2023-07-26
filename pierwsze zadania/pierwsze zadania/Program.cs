3907d5987c6bb613f7b5e80c0f2f561a75bd64a6
maniek maniek <km1265341@gmail.com>
2023-07-26 17:48:32 +0200

    n

--- pierwsze zadania/pierwsze zadania/Program.cs
+++ pierwsze zadania/pierwsze zadania/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int myAge = 18;
+int newAge = myAge + 5;
+float idk = 18.5f;
+string imię = "Jakub ";
+string nazwisko = "Mańka ";
+string wynik = imię+nazwisko;
+Console.WriteLine(wynik+myAge);
+char nw = 't';
+Console.WriteLine(nw);
+
+
+bool isActive = true;
+isActive = false;
+var inValid = 5 > 6;
+Console.WriteLine(isActive);
