518e9663b15de6690a3c88ce93a375c66d59a0d7
maniek maniek <km1265341@gmail.com>
2023-07-29 10:43:20 +0200

    zadanie domowe dzień 4

---
+++ dzień 4 projekt/dzień 4 projekt/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "Karolina";
+string gender = "Woman";
+int age = 18;
+
+
+if (gender == "Woman" && age<30) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if ( gender != "Woman" && age < 18 )
+{
+    Console.WriteLine("Niepełnoletni męszczyzna");
+}
